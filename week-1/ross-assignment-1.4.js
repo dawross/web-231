ee2d93aefa8540395475b0033ee4c1db1f7c1ce6
Dan Ross <dawross@my365.bellevue.edu>
2020-05-30 21:01:05 -0500

    assignment 1.4

---
+++ week-1/ross-assignment-1.4.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Dan Ross
+; Date: 5 May 2020
+; Modified By: Dan Ross
+; Description: The purpose of this exercise 
+; is to provide a basic demonstration of how 
+; to run a JavaScript program in VS Code.
+;===========================================
+*/
+
+var firstName = "Dan";
+var lastName = "Ross";
+var age = "46";
+var street = "1234 Random Street";
+var city = "Ames";
+var zipCode = "50010";
+
+//These will print the variables to the console. 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
+
