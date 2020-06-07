409ef92a9e5772689f39403ddaa45736d37ec7c5
Dan Ross <dawross@my365.bellevue.edu>
2020-06-06 20:03:24 -0500

    Exercise 2.2

--- week-2/Ross-header-test.js
+++ week-2/Ross-header-test.js
@@ -1,4 +1,14 @@
 
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   6 June 2020
+; Modified By: Dan Ross
+; Description: Displays a formatted header
+;===========================================
+*/
+
 const header = require('../Ross-header.js');
 
 console.log(header.display("Dan", "Ross", "Exercise 2.2 - Header"));
