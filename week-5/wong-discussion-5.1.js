38111181c8e75bc9b036b3c8479e2fae2a596f1d
Dan Ross <dawross@my365.bellevue.edu>
2020-06-25 20:11:48 -0500

    Adding discussion  5.1

--- week-5/wong-discussion-5.1.js
+++ week-5/wong-discussion-5.1.js
@@ -3,7 +3,7 @@
 ; Title: Arrays js
 ; Author: Devan Wong
 ; Date: 25 June 2020
-; Modified By:
+; Modified By: Dan Ross
 ; Description: Find at least two errors.
 ;===========================================
 */
@@ -14,7 +14,7 @@ inventory.set('paper towel', 3);
 inventory.set('tissue', 0);
 inventory.set('sanitizer', 2);
 
-for (let [value, key] of inventory) { //replaced the () around "value, key" with [] since it's looping through an array
+for (let [value, key] of inventory) { //DR - replaced the () around "value, key" with [] since it's looping through an array
   if (value < 5) {
     console.log(key + ' is running low. You have ' + value + ' left.');
   }
