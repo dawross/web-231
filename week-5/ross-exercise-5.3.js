e6efad9031bfc1a3ff0d05522fda4bea9cd4488e
Dan Ross <dawross@my365.bellevue.edu>
2020-06-26 20:45:02 -0500

    Adding exercise 5.3

---
+++ week-5/ross-exercise-5.3.js
@@ -0,0 +1,59 @@
+/*
+============================================
+; Title: Exercise 5.3
+; Author: Dan Ross
+; Date: 26 June 2020;
+; Description: Object Collections
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require('../ross-header.js');
+
+//composers object
+var composers = [
+  {
+    firstName: "Ludwig van",
+    lastName: "Beethoven",
+    genre: "Classical",
+    rating: "8"
+  },
+  {
+    firstName: "Wolfgang",
+    lastName: "Mozart",
+    genre: "Classical",
+    rating: "10"
+  },
+  {
+    firstName: "Johann",
+    lastName: "Bach",
+    genre: "Classical",
+    rating: "9"
+  },
+  {
+    firstName: "Franz",
+    lastName: "Haydn",
+    genre: "Classical",
+    rating: "6"
+  },
+  {
+    firstName: "Franz",
+    lastName: "Schubert",
+    genre: "Classical",
+    rating: "5"
+  }
+]
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 5.3"));
+//new line
+console.log('');
+
+//iterate over composer object and output results
+composers.forEach(function(e){
+  console.log("Last Name: " + e.lastName + ", " + "Genre: " + e.genre + ", " + "Rating: " + e.rating);
+});
+
+
+
+
+
+
