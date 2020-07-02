54ee71a12bd7c22a8bf98539d60de53a9d7daf4b
Dan Ross <dawross@my365.bellevue.edu>
2020-07-02 15:25:39 -0500

    Adding duscussion 6.1

--- week-6/ross-discussion-6.1.js
+++ week-6/ross-discussion-6.1.js
@@ -7,6 +7,8 @@
 ;===========================================
 */
 //This program has 3 errors
+
+//Object
 var book = {
 	"title": "Weaveworld",
   "for": "mature audiences",
@@ -17,6 +19,9 @@ var book = {
     lastName: "Barker"
 
 };
+
+//output
 console.log("One of my favorite books is " + book.title + " by " + book.firstName + " " + author.lastName + ". " + "It's for " + book.for + " and is a " + book.genre + " novel. " + "Its ISBN is " + book.ISBN + ".");
 
+//Expected output
 //One of my favorite books is Weaveworld by Clive Barker. It's for mature audiences and is a gothic fiction novel. Its ISBN is 0743417356.
