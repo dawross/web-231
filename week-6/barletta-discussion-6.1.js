02170b420bdcb080b73b5307a13c0ddca28602f1
Dan Ross <dawross@my365.bellevue.edu>
2020-07-04 13:17:46 -0500

    Adding discussion 6.1

---
+++ week-6/barletta-discussion-6.1.js
@@ -0,0 +1,40 @@
+
+/*
+  	============================================
+  	; Title: barleta-fixErrors.js
+  	; Author: Marie Nicole Barleta
+  	; Date: 02 July 2020
+  	; Modified by:
+  	; Description: Find errors, objects in JavaScript
+  	;===========================================
+  	*/
+
+  	/*
+  	Expected output:
+
+  	Employee ID: 152942
+  	Employee name: Nicole
+  	Employee rate: 15
+  	Employee name: Full-time
+
+  	*/
+
+
+
+  	//find errors
+  	var employees = {
+
+  	 idNum: 0452556,
+
+  	 id: 152942, //DR - added id of 152942
+  	 name: "Nicole",
+  	 rate: 15, //DR - added comma
+  	 types: "Full-time"
+
+    }// DR - added closing curly bracket
+
+
+  	console.log("Employee ID: " + employees.id) //DR - changed , to +. Deleted quotes around employees.id. Changed idNum to id based on expected output.
+  	console.log("Employee name: " + employees.name) //DR - got rid of brackets around name
+  	console.log("Employee rate: " + employees.rate)// DR - changed "employees.['id']['rate']" to "employees.rate"
+  	console.log("Employee name: " + employees.type)// DR - Deleted quotes around employees.type. Not sure why this one still says "Employee Name", but I left is as-is based on expected output.
