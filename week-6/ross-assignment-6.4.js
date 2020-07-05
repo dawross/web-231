821652cc3155eb41afbaa2aa657b7d234a8aa9e3
Dan Ross <dawross@my365.bellevue.edu>
2020-07-05 15:00:56 -0500

    Adding assignment 6.4

---
+++ week-6/ross-assignment-6.4.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: Assignment 6.4
+; Author: Dan Ross
+; Date: 5 July 2020;
+; Description: Nested Object Literals
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require('../ross-header.js');
+
+// output header
+console.log(header.display("Dan", "Ross", "Assignment 6.4"));
+console.log('');
+
+//object
+var ticket = {
+  id: 105,
+  name: "Help Desk Support",
+  requester: "Dan Ross",
+  dateCreated: "07/05/2020",
+  priority: "medium",
+  person:{
+    id: 211,
+    firstName: "Bob",
+    lastName: "Jones",
+    jobTitle: "Programmer I"
+  }
+}
+//output
+console.log("Ticket " + ticket.id + " was created on " + ticket.dateCreated + " and assigned to employee " + ticket.person.firstName + " " + ticket.person.lastName + " (" + ticket.person.jobTitle + ").")
