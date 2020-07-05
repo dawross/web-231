71bcff084ca23b297ceefebe0c70b7d630737529
Dan Ross <dawross@my365.bellevue.edu>
2020-07-05 13:46:27 -0500

    Adding exercise 6.2

---
+++ week-6/ross-exercise-6.2.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Exercise 6.2
+; Author: Dan Ross
+; Date: 5 July 2020;
+; Description: Exception Handling
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require('../ross-header.js');
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 6.2
+  Today's Date
+
+  Catch clause: <your custom message goes here>
+  Finally clause reached...
+*/
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 6.2"));
+console.log('');
+
+try {
+  //variables
+  var x = "1";
+  var y = "7"
+  //if statement
+  if (x + y !== '9'){
+    throw new Error("This sum does not equal 9")
+  }
+} catch(e) {
+  //output
+  console.log("Catch clause: " + e.message)
+} finally {
+  console.log("Finally clause reached...")
+}
