08190d9cc1e6800d74564ed349f22cca3c908af7
Dan Ross <dawross@my365.bellevue.edu>
2020-07-05 14:36:53 -0500

    Adding exercise 6.3

---
+++ week-6/ross-exercise-6.3.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Exercise 6.2
+; Author: Dan Ross
+; Date: 5 July 2020;
+; Description: Exception Handling
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require('../ross-header.js');
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 6.3"));
+console.log('');
+//object
+var ticket = {
+  id: 101,
+  name: "Help Desk Support",
+  requester: "Bob Jones"
+}
+//output
+console.log("{id: " + ticket.id + ", name: " + ticket.name + ", requestor: " + ticket.requester + "}");
