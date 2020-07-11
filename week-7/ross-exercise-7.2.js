f2bf79eef086bd14c58e3fa99f2e50d68abe7f8f
Dan Ross <dawross@my365.bellevue.edu>
2020-07-11 18:13:00 -0500

    adding exercise 7.2

---
+++ week-7/ross-exercise-7.2.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: Exercise 7.2
+; Author: Dan Ross
+; Date: 11 July 2020;
+; Description: Constructor Functions
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require('../ross-header.js');
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 7.2"));
+console.log('');
+
