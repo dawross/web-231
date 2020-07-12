260fb77ac37dc6e4f204f701c12e50f35f8047f8
Dan Ross <dawross@my365.bellevue.edu>
2020-07-11 19:10:18 -0500

    adding exercise 7.2

--- week-7/ross-exercise-7.2.js
+++ week-7/ross-exercise-7.2.js
@@ -9,7 +9,32 @@
 //Imports the ross-header.js file from the root directory
 const header = require('../ross-header.js');
 
+/**
+ * Params: id, firstName, lastName, title
+ * Response: void
+ * Description: sets the id, first name, last name, and title an object
+ */
+function employee(id, firstName, lastName, title) {
+  this.id = id;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+}
+//create 5 new objects
+var employees = [
+  new employee("1", "Thomas", "Edison", "Software Engineer"),
+  new employee("2", "Benjamin", "Franklin", "Programmer"),
+  new employee("3", "Nikola", "Tesla", "Project Manager"),
+  new employee("4", "Charles", "Babbage", "Product Manager"),
+  new employee("5", "Alexander", "Bell", "Business Analyst"),
+]
+
 // output header
 console.log(header.display("Dan", "Ross", "Exercise 7.2"));
 console.log('');
 
+//loop through array and output results
+for (var i = 0; i < employees.length; i++) {
+  console.log(employees[i].id + " " + employees[i].firstName + " " + employees[i].lastName + " " + employees[i].title);
+
+}
